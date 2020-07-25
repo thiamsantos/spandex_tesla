89598e7b61392beaedfe34ed45ceb9312150b72e
Thiago Santos <thia.mdossantos@gmail.com>
2020-07-25 11:20:49 -0300

    1.0.0

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ by adding `spandex_tesla` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:spandex_tesla, "~> 0.1.0"}
+    {:spandex_tesla, "~> 1.0.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SpandexTesla.MixProject do
   def project do
     [
       app: :spandex_tesla,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

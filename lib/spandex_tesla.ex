56ef07be71cbd627d0fc32d0ae8d791e4b74c59b
Marcelo De Polli <mdepolli@users.noreply.github.com>
2021-07-12 13:54:48 -0300

    Add support for Tesla >= 1.3.3 (#7)
    This PR adds support for Tesla 1.3.3 and greater.
    
    **Changes**
    
    - Handle new Telemetry events: `[:tesla, :request, :start]`, `[:tesla, :request, :stop]`, `[:tesla, :request, :exception]`
    - Use `:native` time on all timestamps
    - Allow easy installation via `SpandexTesla.Telemetry.attach()`
    
    Fixes: https://github.com/thiamsantos/spandex_tesla/issues/6

--- lib/spandex_tesla.ex
+++ lib/spandex_tesla.ex
@@ -28,12 +28,7 @@ defmodule SpandexTesla do
 
   ```elixir
   # in application.ex
-  :telemetry.attach(
-    "spandex-tesla-tracer",
-    [:tesla, :request],
-    &SpandexTesla.handle_event/4,
-    nil
-  )
+  SpandexTesla.Telemetry.attach()
   ```
 
   """
@@ -48,6 +43,56 @@ defmodule SpandexTesla do
   @doc """
   Telemetry handler. Attach it to the telemetry tesla events in order to trace the tesla calls.
   """
+  def handle_event([:tesla, :request, :start], _measurements, _metadata, _config) do
+    if tracer().current_trace_id([]) do
+      tracer().start_span("request", [])
+
+      Logger.metadata(
+        trace_id: to_string(tracer().current_trace_id([])),
+        span_id: to_string(tracer().current_span_id([]))
+      )
+    end
+  end
+
+  def handle_event([:tesla, :request, :stop], measurements, metadata, _config) do
+    if tracer().current_trace_id([]) do
+      now = clock_adapter().system_time()
+      %{duration: duration} = measurements
+      %{status: status, url: url, method: method} = metadata[:env]
+      upcased_method = method |> to_string() |> String.upcase()
+
+      tracer().update_span(
+        start: now - duration,
+        completion_time: now,
+        service: service(),
+        resource: "#{upcased_method} #{url}",
+        type: :web,
+        http: [
+          url: url,
+          status_code: status,
+          method: upcased_method
+        ]
+      )
+
+      tracer().finish_span([])
+    end
+  end
+
+  def handle_event([:tesla, :request, :exception], _measurements, metadata, _config) do
+    if tracer().current_trace_id([]) do
+      reason = metadata[:reason] || metadata[:error]
+
+      tracer().span_error(%Error{message: inspect(reason)}, nil, [])
+
+      tracer().finish_span([])
+
+      Logger.metadata(
+        trace_id: to_string(tracer().current_trace_id([])),
+        span_id: to_string(tracer().current_span_id([]))
+      )
+    end
+  end
+
   def handle_event([:tesla, :request], measurements, metadata, _) do
     if tracer().current_trace_id([]) do
       now = clock_adapter().system_time() |> System.convert_time_unit(:native, :nanosecond)
--- test/spandex_tesla_test.exs
+++ test/spandex_tesla_test.exs
@@ -5,7 +5,91 @@ defmodule SpandexTeslaTest do
 
   setup :verify_on_exit!
 
-  describe "handle_event/4" do
+  describe "handle_event/4 with new telemetry events" do
+    test "skip span when there is no trace_id" do
+      duration = 1_000
+
+      expect(TracerMock, :current_trace_id, 2, fn _ -> nil end)
+
+      SpandexTesla.handle_event(
+        [:tesla, :request, :start],
+        nil,
+        nil,
+        nil
+      )
+
+      SpandexTesla.handle_event(
+        [:tesla, :request, :stop],
+        %{duration: duration},
+        %{env: %{status: 200, url: "https://google.com", method: :get}},
+        nil
+      )
+    end
+
+    test "span tesla request success result" do
+      now = System.system_time()
+      duration = 1_000
+      trace_id = "trace_id"
+      span_id = "span_id"
+
+      ClockMock
+      |> expect(:system_time, fn -> now end)
+
+      TracerMock
+      |> expect(:current_trace_id, 3, fn [] -> trace_id end)
+      |> expect(:current_span_id, fn [] -> span_id end)
+      |> expect(:start_span, fn "request", [] -> nil end)
+      |> expect(:update_span, fn opts ->
+        assert opts[:start] == now - duration
+        assert opts[:completion_time] == now
+        assert opts[:service] == :tesla
+        assert opts[:resource] == "GET https://google.com"
+        assert opts[:type] == :web
+        assert opts[:http] == [url: "https://google.com", status_code: 200, method: "GET"]
+      end)
+      |> expect(:finish_span, fn [] -> nil end)
+
+      SpandexTesla.handle_event(
+        [:tesla, :request, :start],
+        nil,
+        nil,
+        nil
+      )
+
+      SpandexTesla.handle_event(
+        [:tesla, :request, :stop],
+        %{duration: duration},
+        %{env: %{status: 200, url: "https://google.com", method: :get}},
+        nil
+      )
+
+      assert Logger.metadata() == [span_id: "span_id", trace_id: "trace_id"]
+    end
+
+    test "span tesla request error result" do
+      trace_id = "trace_id"
+      span_id = "span_id"
+
+      TracerMock
+      |> expect(:current_trace_id, 2, fn [] -> trace_id end)
+      |> expect(:current_span_id, fn [] -> span_id end)
+      |> expect(:span_error, fn error, nil, [] ->
+        assert error == %SpandexTesla.Error{message: inspect(:timeout)}
+      end)
+      |> expect(:finish_span, fn [] -> nil end)
+
+      SpandexTesla.handle_event(
+        [:tesla, :request, :exception],
+        %{duration: 1_000},
+        %{reason: :timeout},
+        nil
+      )
+
+      assert Logger.metadata() == [span_id: "span_id", trace_id: "trace_id"]
+    end
+  end
+
+  describe "handle_event/4 with legacy telemetry events" do
     test "skip span when there is no trace_id" do
       expect(TracerMock, :current_trace_id, fn _ -> nil end)
 
